ec609d228d40e79c8e4265057d0d2cf5d781f267
tarikgul <tariksnow37@gmail.com>
2025-05-04 12:09:22 -0400

    Fix parameters for abstractApi

--- backend/src/services/abstractApi.ts
+++ backend/src/services/abstractApi.ts
@@ -1,6 +1,11 @@
 import { ApiPromise, WsProvider } from '@polkadot/api';
 
-export const abstractApi = async (url: string) => {
+import { getConfig } from '../config';
+
+export const abstractApi = async (type: 'asset-hub' | 'relay-chain') => {
+    const config = getConfig();
+    const url = type === 'asset-hub' ? config.assetHubUrl : config.relayChainUrl;
+
     const api = await ApiPromise.create({
         provider: new WsProvider(url),
     });
