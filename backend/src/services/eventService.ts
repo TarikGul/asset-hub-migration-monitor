69839f94921f50945f7c91fce91037c4a2b60ab8
tarikgul <tariksnow37@gmail.com>
2025-05-06 21:16:48 -0400

    Create singleton Event Emitter

---
+++ backend/src/services/eventService.ts
@@ -0,0 +1,19 @@
+import { EventEmitter } from 'events';
+
+// Create a singleton event emitter
+class EventService extends EventEmitter {
+  private static instance: EventService;
+
+  private constructor() {
+    super();
+  }
+
+  public static getInstance(): EventService {
+    if (!EventService.instance) {
+      EventService.instance = new EventService();
+    }
+    return EventService.instance;
+  }
+}
+
+export const eventService = EventService.getInstance(); 
