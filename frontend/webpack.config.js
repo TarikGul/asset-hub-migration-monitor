afd275ba88ab2f4970d4a35208c6dea2867f5ff4
tarikgul <tariksnow37@gmail.com>
2025-05-09 21:48:56 -0400

    Add tsconfig, and webpack for frontend

---
+++ frontend/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    port: 3000,
+    hot: true,
+    proxy: [{
+      context: ['/'],
+      target: 'http://localhost:8080',
+      secure: false,
+      changeOrigin: true,
+      bypass: (req) => {
+        // Don't proxy requests for static assets
+        if (req.url.match(/\.(js|css|html|png|jpg|jpeg|gif|ico|svg)$/)) {
+          return req.url;
+        }
+      },
+    }],
+  },
+}; 
